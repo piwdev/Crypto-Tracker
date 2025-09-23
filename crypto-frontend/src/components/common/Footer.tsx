9fb595552a72cd7ea1ec3bc637b4b082ad6a1fe3
piwdev <eatdrink0507@gmail.com>
2025-09-23 13:57:20 +0900

    fix : Footer's message

--- crypto-frontend/src/components/common/Footer.tsx
+++ crypto-frontend/src/components/common/Footer.tsx
@@ -3,15 +3,20 @@ import { useTranslation } from 'react-i18next';
 import './Footer.css';
 
 const Footer: React.FC = () => {
-    
+
     const { t } = useTranslation();
 
     return (
         <footer className="footer">
             <div className="footer-content">
-                <p className="footer-text">
-                    {t('footer.dataSource')}
-                </p>
+                {
+                    t('footer.aboutSite').split('\r\n').map((line: string, index: number) => (
+                        <p key={index} className='footer-text'>
+                            {line}
+                            {index===0 ? <a href={t('footer.dataSourceURL')} target='_blank'>{t('footer.dataSourceName')}</a> : null }
+                            </p>
+                    ))
+                }
             </div>
         </footer>
     );
